b929d24e2ab5e78c812b3605721112920df2f9fa
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-05-13 11:54:55 +0200

    update

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://riaanbredenhann:XRtXCWmmKhXmwPwL@book-store-mern.ewplpto.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN';
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send("Welcome to MERN stack tutorial");
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
