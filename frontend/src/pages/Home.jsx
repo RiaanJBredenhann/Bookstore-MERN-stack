5814c53319a891cc637a94b2169139f1e590f0c2
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-05-20 15:08:09 +0200

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
               <th className='border border-slate-600 rounded-md'>No</th>
               <th className='border border-slate-600 rounded-md'>Title</th>
               <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Published Year</th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
               <th className='border border-slate-600 rounded-md'>Operations</th>
             </tr>
           </thead>
