4b159da724e2718f9c1740e3c2028a256b05cb62
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-05-21 13:01:25 +0200

    update

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const CreateBooks = () => {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
-    const [publushYear, setPublishYear] = useState('');
+    const [publishYear, setPublishYear] = useState('');
     const [loading, setloading] = useState(false);
     const navigate = useNavigate();
 
@@ -40,28 +40,28 @@ const CreateBooks = () => {
                 <div className="my-4">
                     <label className="text-xl mr-4 text-gray-500">Title</label>
                     <input
-                        type="text" 
-                        value={title} 
-                        onChange={(e) => setTitle(e.target.value)} 
-                        className="border-2 border-gray-500 px-4 py-2 w-full" 
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="border-2 border-gray-500 px-4 py-2 w-full"
                     />
                 </div>
                 <div className="my-4">
                     <label className="text-xl mr-4 text-gray-500">Author</label>
                     <input
-                        type="text" 
-                        value={author} 
-                        onChange={(e) => setAuthor(e.target.value)} 
-                        className="border-2 border-gray-500 px-4 py-2 w-full" 
+                        type="text"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                        className="border-2 border-gray-500 px-4 py-2 w-full"
                     />
                 </div>
                 <div className="my-4">
                     <label className="text-xl mr-4 text-gray-500">Publish Year</label>
                     <input
-                        type="text" 
-                        value={publishYear} 
-                        onChange={(e) => setPublishYear(e.target.value)} 
-                        className="border-2 border-gray-500 px-4 py-2 w-full" 
+                        type="text"
+                        value={publishYear}
+                        onChange={(e) => setPublishYear(e.target.value)}
+                        className="border-2 border-gray-500 px-4 py-2 w-full"
                     />
                 </div>
                 <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>Save</button>
